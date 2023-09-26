f687d6671a2eb39679824a8bb4a544ae212405d6
mardhika wulansari <64689649+ichalatte@users.noreply.github.com>
2023-09-26 19:34:57 +0700

    video handling

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const apiRouter = require("express").Router();
 const api = require("../routes/api");
 const imageRoutes = require('../routes/api/image.routes');
+const videoRoutes = require('../routes/api/video.routes')
 
 apiRouter.get("/", (req, res) => {
     res.send("test");
@@ -8,5 +9,6 @@ apiRouter.get("/", (req, res) => {
 
 apiRouter.use("/api", api);
 apiRouter.use("/image", imageRoutes);
+apiRouter.use("/video", videoRoutes);
 
 module.exports = apiRouter;
