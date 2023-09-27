cbc177c70422a1407af96b3c74a668c6f884e0c0
Shendy <Shendy1567@gmail.com>
2023-09-27 16:21:30 +0700

    adding test to auth controller

--- controllers/auth.contoller.js
+++ controllers/auth.contoller.js
@@ -3,122 +3,150 @@ const { hashPassword, verifyPassword } = require("../utils/passwordHandler");
 const jwt = require("jsonwebtoken");
 
 module.exports = class AuthContollers {
-    async registerPlayer(req, res, next) {
-        const { username, email, password, confPassword } = req.body;
-        if (await User.findOne({ where: { email } }))
-            return res.status(400).json({ msg: "Email sudah di gunakan" });
-        if (password !== confPassword)
-            return res.status(400).json({ msg: "Password tidak sama" });
-        const newPassword = await hashPassword(password);
+  async registerPlayer(req, res, next) {
+    const { username, email, password, confPassword } = req.body;
 
-        try {
-            await User.create({
-                username: username,
-                email: email,
-                password: newPassword,
-                role: "user",
-                profile_image_url: "https://res.cloudinary.com/dz0cuhiny/image/upload/v1695553065/ldktrzeo1oud4iidzx9f.jpg"
-            });
-            res.json({ msg: "Register player berhasil" });
-        } catch (error) {
-            next(error);
-        }
+    if (await User.findOne({ where: { email } }))
+      return res.status(400).json({ msg: "Email sudah di gunakan" });
+
+    if (password !== confPassword)
+      return res.status(400).json({ msg: "Password tidak sama" });
+    const newPassword = await hashPassword(password);
+
+    try {
+      await User.create({
+        username: username,
+        email: email,
+        password: newPassword,
+        role: "user",
+        profile_image_url:
+          "https://res.cloudinary.com/dz0cuhiny/image/upload/v1695553065/ldktrzeo1oud4iidzx9f.jpg",
+      });
+      res.status(200);
+      res.json({ msg: "Register player berhasil" });
+    } catch (error) {
+      return res.status(500).json({
+        result: "Error",
+        error: error.message,
+      });
     }
+  }
 
-    async loginPlayer(req, res) {
-        try {
-            const user = await User.findOne({
-                where: {
-                    email: req.body.email,
-                },
-            });
+  async loginPlayer(req, res) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
 
-            const matchPassword = await verifyPassword(
-                req.body.password,
-                user.password
-            );
-            if (!matchPassword)
-                return res.status(400).json({ msg: "Password Salah" });
+      const matchPassword = await verifyPassword(
+        req.body.password,
+        user.password
+      );
+      if (!matchPassword)
+        return res.status(400).json({ msg: "Password Salah" });
 
-            const userId = user.id;
-            const username = user.username;
-            const email = user.email;
+      const userId = user.id;
+      const username = user.username;
+      const email = user.email;
 
-
-            const accessToken = jwt.sign({ userId, username, email },
-                process.env.ACCESS_TOKEN, {
-                    expiresIn: "20s",
-                }
-            );
-
-            const refreshToken = jwt.sign({ userId, username, email },
-                process.env.REFRESH_TOKEN, {
-                    expiresIn: "1d",
-                }
-            );
-
-            await User.update({ refresh_token: refreshToken }, {
-                where: {
-                    id: userId,
-                },
-            });
-
-            res.cookie("refreshToken", refreshToken, {
-                maxAge: 24 * 60 * 60 * 1000,
-            });
-
-            res.json({ accessToken });
-        } catch (error) {
-            res.status(404).json({ msg: "Email tidak ditemukan" });
+      const accessToken = jwt.sign(
+        { userId, username, email },
+        process.env.ACCESS_TOKEN,
+        {
+          expiresIn: "20s",
         }
-    }
+      );
 
-    async logoutPlayer(req, res) {
-        const refreshToken = req.cookies.refreshToken;
-        if (!refreshToken) return res.sendStatus(204);
-        const user = await User.findOne({
-            where: {
-                refresh_token: refreshToken,
-            },
-        });
-        if (!user) return res.sendStatus(204);
-        const userId = user.id;
-        await User.update({ refresh_token: null }, {
-            where: {
-                id: userId,
-            },
-        });
+      const refreshToken = jwt.sign(
+        { userId, username, email },
+        process.env.REFRESH_TOKEN,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      await User.update(
+        { refresh_token: refreshToken },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+
+      res.cookie("refreshToken", refreshToken, {
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+
+      res.status(200);
+      res.json({ accessToken });
+    } catch (error) {
+      res.status(404).json({ msg: "Email tidak ditemukan" });
+    }
+  }
+
+  async logoutPlayer(req, res) {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.sendStatus(204);
+    const user = await User.findOne({
+      where: {
+        refresh_token: refreshToken,
+      },
+    });
+    if (!user) return res.sendStatus(204);
+    const userId = user.id;
+    await User.update(
+      { refresh_token: null },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    res.clearCookie("refreshToken");
+    return res.sendStatus(200);
+  }
+
+  async refreshToken(req, res, next) {
+    try {
+      const refreshToken = req.cookies.refreshToken;
+      if (!refreshToken) return res.sendStatus(204);
+      const user = await User.findOne({
+        where: {
+          refresh_token: refreshToken,
+        },
+      });
+      if (!user) {
+        res.sendStatus(404);
         res.clearCookie("refreshToken");
-        return res.sendStatus(200);
-    }
-
-    async refreshToken(req, res, next) {
-        try {
-            const refreshToken = req.cookies.refreshToken;
-            if (!refreshToken) return res.sendStatus(200);
-            const user = await User.findOne({
-                where: {
-                    refresh_token: refreshToken,
-                },
-            });
-            // console.log("refresh token is " + refreshToken);
-            if (!user) res.clearCookie("refreshToken").sendStatus(403);
-            jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, decoded) => {
-                if (err) return res.clearCookie("refreshToken").sendStatus(403);
-                const userId = user.id;
-                const username = user.username;
-                const email = user.email;
-                const picture = user.profile_image_url;
-
-                const accessToken = jwt.sign({ userId, username, email, picture },
-                    process.env.ACCESS_TOKEN, {
-                        expiresIn: "15s",
-                    }
-                );
-                res.json({ accessToken });
-            });
-        } catch (error) {
-            next(error);
+      }
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, decoded) => {
+        if (err) {
+          res.clearCookie("refreshToken");
+          res.sendStatus(403);
         }
+        const userId = user.id;
+        const username = user.username;
+        const email = user.email;
+        const picture = user.profile_image_url;
+
+        const accessToken = jwt.sign(
+          { userId, username, email, picture },
+          process.env.ACCESS_TOKEN,
+          {
+            expiresIn: "15s",
+          }
+        );
+        res.sendStatus(200);
+        res.json({ accessToken });
+      });
+    } catch (error) {
+      return res.status(500).json({
+        result: "Error",
+        error: error.message,
+      });
     }
+  }
 };
---
+++ tests/auth.controller.spec.js
@@ -0,0 +1,478 @@
+const AuthController = require("../controllers/auth.contoller");
+const { registerPlayer, loginPlayer, refreshToken, logoutPlayer } =
+  new AuthController();
+const { hashPassword, verifyPassword } = require("../utils/passwordHandler");
+const jwt = require("jsonwebtoken");
+const { User } = require("../database/models");
+
+jest.mock("../utils/passwordHandler");
+jest.mock("jsonwebtoken");
+
+describe("Register new User /api/auth/register", () => {
+  it("post new user to database then return status: 200, msg: register berhasil", async () => {
+    const req = {
+      body: {
+        username: "test",
+        email: "test@email.com",
+        password: "tested",
+        confPassword: "tested",
+      },
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(null);
+    const newPassword = await hashPassword(req.body.password);
+    User.create = jest.fn().mockResolvedValue({
+      username: req.body.username,
+      email: req.body.email,
+      password: newPassword,
+      role: "user",
+      profile_image_url: "test picture",
+    });
+
+    await registerPlayer(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: "Register player berhasil",
+    });
+  });
+
+  it("should return status 400 because email is already used", async () => {
+    const req = {
+      body: {
+        username: "test",
+        email: "test@email.com",
+        password: "tested",
+        confPassword: "tested",
+      },
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne = jest
+      .fn()
+      .mockResolvedValue({ where: { email: req.body.email } });
+    const newPassword = await hashPassword(req.body.password);
+    User.create = jest.fn().mockResolvedValue({
+      username: req.body.username,
+      email: req.body.email,
+      password: newPassword,
+      role: "user",
+      profile_image_url: "test picture",
+    });
+
+    await registerPlayer(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: "Email sudah di gunakan",
+    });
+  });
+
+  it("should return status 400 because password and the confirmation is not the same", async () => {
+    const req = {
+      body: {
+        username: "test",
+        email: "test@email.com",
+        password: "tested",
+        confPassword: "notsame",
+      },
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(null);
+    const newPassword = await hashPassword(req.body.password);
+    User.create = jest.fn().mockResolvedValue({
+      username: req.body.username,
+      email: req.body.email,
+      password: newPassword,
+      role: "user",
+      profile_image_url: "test picture",
+    });
+
+    await registerPlayer(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: "Password tidak sama",
+    });
+  });
+
+  it("should handle errors on post /api/auth/register", async () => {
+    const req = {
+      body: {
+        username: "test",
+        email: "test@email.com",
+        password: "tested",
+        confPassword: "tested",
+      },
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(null);
+    User.create = jest.fn().mockRejectedValue(new Error("Database error"));
+
+    await registerPlayer(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      result: "Error",
+      error: "Database error",
+    });
+  });
+});
+
+describe("Login User /api/auth/login", () => {
+  it("post user username and password for login to database then return status: 200 and the access token", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "tested",
+      },
+    };
+
+    const mockUser = {
+      id: 1,
+      username: "test",
+      email: "test@example.com",
+      password: "hashedPassword",
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+      cookie: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    verifyPassword.mockResolvedValue(true);
+
+    jest.spyOn(jwt, "sign").mockReturnValue("mockedAccessToken");
+
+    await loginPlayer(req, res);
+
+    expect(jwt.sign).toHaveBeenCalledWith(
+      {
+        userId: mockUser.id,
+        username: mockUser.username,
+        email: mockUser.email,
+      },
+      process.env.ACCESS_TOKEN,
+      { expiresIn: "20s" }
+    );
+    expect(jwt.sign).toHaveBeenCalledWith(
+      {
+        userId: mockUser.id,
+        username: mockUser.username,
+        email: mockUser.email,
+      },
+      process.env.REFRESH_TOKEN,
+      { expiresIn: "1d" }
+    );
+
+    expect(res.cookie).toHaveBeenCalledWith(
+      "refreshToken",
+      "mockedAccessToken",
+      {
+        maxAge: 24 * 60 * 60 * 1000,
+      }
+    );
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ accessToken: "mockedAccessToken" });
+  });
+
+  it("password is not matching so return status 400 and msg password salah", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "tested",
+      },
+    };
+
+    const mockUser = {
+      id: 1,
+      username: "test",
+      email: "test@example.com",
+      password: "hashedPassword",
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    verifyPassword.mockResolvedValue(false);
+
+    await loginPlayer(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ msg: "Password Salah" });
+  });
+
+  it("should handle errors on post /api/auth/login", async () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "tested",
+      },
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(null);
+    User.create = jest.fn().mockRejectedValue(new Error("Database error"));
+
+    verifyPassword.mockResolvedValue(true);
+
+    await loginPlayer(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: "Email tidak ditemukan",
+    });
+  });
+});
+
+describe("Logout User /api/auth/logout", () => {
+  it("it should logging out user and clear its refresh token", async () => {
+    const req = {
+      cookies: { refreshToken: "mockRefreshToken" },
+    };
+
+    const mockUser = {
+      id: 1,
+      refresh_token: "mockRefreshToken",
+    };
+
+    const res = {
+      sendStatus: jest.fn(),
+      clearCookie: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    await logoutPlayer(req, res);
+
+    expect(res.clearCookie).toHaveBeenCalledWith("refreshToken");
+    expect(res.sendStatus).toHaveBeenCalledWith(200);
+  });
+
+  it("it's not logging out user because didnt have token", async () => {
+    const req = {
+      cookies: { refreshToken: null },
+    };
+
+    const mockUser = {
+      id: 1,
+      refresh_token: "mockRefreshToken",
+    };
+
+    const res = {
+      sendStatus: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    await logoutPlayer(req, res);
+
+    expect(res.sendStatus).toHaveBeenCalledWith(204);
+  });
+
+  it("it's not logging out user because token is different", async () => {
+    const req = {
+      cookies: { refreshToken: "different Token" },
+    };
+
+    const res = {
+      sendStatus: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(null);
+
+    await logoutPlayer(req, res);
+
+    expect(res.sendStatus).toHaveBeenCalledWith(204);
+  });
+});
+
+describe("Load RefreshToken /api/auth/token", () => {
+  it("it make new refresh token that can be used", async () => {
+    const req = {
+      cookies: { refreshToken: "mockRefreshToken" },
+    };
+
+    const mockUser = {
+      id: 1,
+      username: "test",
+      email: "test@example.com",
+      picture: "test picture",
+      refresh_token: "mockRefreshToken",
+    };
+
+    const res = {
+      sendStatus: jest.fn(() => res),
+      json: jest.fn(),
+      clearCookie: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    const decodedToken = {
+      userId: mockUser.id,
+      username: mockUser.username,
+      email: mockUser.email,
+      picture: mockUser.profile_image_url,
+    };
+
+    jwt.verify.mockImplementation((token, secret, callback) => {
+      callback(null, decodedToken);
+    });
+
+    await refreshToken(req, res);
+
+    jest.spyOn(jwt, "sign").mockReturnValue("mockedAccessToken");
+
+    expect(res.sendStatus).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ accessToken: "mockedAccessToken" });
+  });
+
+  it("it didnt make new token because didnt have token", async () => {
+    const req = {
+      cookies: { refreshToken: null },
+    };
+
+    const mockUser = {
+      id: 1,
+      username: "test",
+      email: "test@example.com",
+      picture: "test picture",
+      refresh_token: "mockRefreshToken",
+    };
+
+    const res = {
+      sendStatus: jest.fn(() => res),
+      json: jest.fn(),
+      clearCookie: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    const decodedToken = {
+      userId: mockUser.id,
+      username: mockUser.username,
+      email: mockUser.email,
+      picture: mockUser.profile_image_url,
+    };
+
+    jwt.verify.mockImplementation((token, secret, callback) => {
+      callback(null, decodedToken);
+    });
+
+    await refreshToken(req, res);
+
+    jest.spyOn(jwt, "sign").mockReturnValue("mockedAccessToken");
+
+    expect(res.sendStatus).toHaveBeenCalledWith(204);
+  });
+
+  it("it didnt make new token because user didnt found", async () => {
+    const req = {
+      cookies: {
+        refreshToken: "invalidRefreshToken",
+      },
+    };
+
+    const res = {
+      sendStatus: jest.fn(),
+      clearCookie: jest.fn(),
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne.mockResolvedValue(null);
+
+    await refreshToken(req, res);
+
+    expect(res.clearCookie).toHaveBeenCalledWith("refreshToken");
+    expect(res.sendStatus).toHaveBeenCalledWith(404);
+  });
+
+  it("it didnt make new token because jwt verivy error", async () => {
+    const req = {
+      cookies: {
+        refreshToken: "validRefreshToken",
+      },
+    };
+
+    const res = {
+      sendStatus: jest.fn(),
+      clearCookie: jest.fn(),
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    const mockUser = {
+      id: 1,
+      username: "test",
+      email: "test@example.com",
+      picture: "test picture",
+      refresh_token: "mockRefreshToken",
+    };
+
+    User.findOne.mockResolvedValue(mockUser);
+
+    jwt.verify.mockImplementation((token, secret, callback) => {
+      callback(new Error("Verification error"), null);
+    });
+
+    await refreshToken(req, res);
+
+    expect(res.clearCookie).toHaveBeenCalledWith("refreshToken");
+    expect(res.sendStatus).toHaveBeenCalledWith(403);
+  });
+
+  it("should handle errors on post /api/auth/token", async () => {
+    const req = {
+      cookies: {
+        refreshToken: "validRefreshToken",
+      },
+    };
+
+    const res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+
+    User.findOne = jest.fn().mockRejectedValue(new Error("Database error"));
+
+    await refreshToken(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      result: "Error",
+      error: "Database error",
+    });
+  });
+});
